393122bdca9d9dc2c1779d4ec5e9a11f52b61abd
Svitlana Lorman <svlormanua@gmail.com>
2024-07-04 22:30:32 +0300

    finish tea page, template for the products' pages

--- app.js
+++ app.js
@@ -3,17 +3,39 @@ const app = express();
 const path = require('path');
 const jsonData = require('./products.json');
 
+//quotes
+let teaQuote = "Після води, чай - найбільш споживаний напій на Землі.";
+let coffeeQuote = "Для пиття використовуються два види кавових зерен: робуста і арабіка. Арабіка має нижчу кислотність" +  
+"і м'якший смак, тоді як робуста більш кисла і гірка.";
 
-// Serve static files from the 'public' directoryapp.use(express.static(path.join(__dirname, 'public')));
+//imgs
+let teaImg = "/pictures/teaImg.png";
+
+// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set EJS as the templating engine
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'));
+app.set('views', path.join(__dirname, 'views'));
 
-// Route to render a random number
+// Route to render the tea page
 app.get('/tea', (req, res) => {
-    res.render('tea', { tea: jsonData.tea });
+    res.render('productItem',
+        { item: jsonData.tea, 
+        title: 'Tea',
+        titleUA: 'ЧАЙ',
+        img: teaImg,
+        quote: teaQuote,
+     });
+});
+app.get('/coffee', (req, res) => {
+    res.render('productItem',
+        { item: jsonData.tea, 
+        title: 'Coffee',
+        titleUA: 'КАВА',
+        img: teaImg,
+        quote: coffeeQuote,
+     });
 });
 
 // Route to render the home page
